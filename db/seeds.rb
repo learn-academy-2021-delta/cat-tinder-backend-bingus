ddc05d2e99b70eb6b2335af8c8fcf28770e32154
Chawwh <chachablueftw@gmail.com>
2021-10-30 07:00:08 -0600

    Recreated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,32 +3,33 @@ cats = [
 	{
 		name: "Felix",
 		age: 2,
-        gender: "male"
-        breed: "Tabby"
+        gender: "male",
+        breed: "Tabby",
 		enjoys: "Long naps by the fire"
 
 	},
 	{
 		name: "Lola",
 		age: 5,
-        gender: "Female"
-        breed: "Tuxedo"
+        gender: "Female",
+        breed: "Tuxedo",
 		enjoys: "hairball fetish"
 	},
 	{
 		name: "Toast",
 		age: 5,
-        gender: "Male"
-        breed: "Calico"
+        gender: "Male",
+        breed: "Calico",
 		enjoys: "warm butter"
 	},
     {
         name: "Cheese Ball",
         age: 1,
-        gender: "Male"
-        breed: "Orange Tabby"
+        gender: "Male",
+        breed: "Orange Tabby",
         enjoys: "Enjoy's being spun by the tail"
     }
+]
 
 
 cats.each do |attributes|
